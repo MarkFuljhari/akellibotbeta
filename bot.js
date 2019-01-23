c30c231ba8d6f5d92872618203b4305d00225398
MarkFuljhari <45743552+MarkFuljhari@users.noreply.github.com>
2019-01-22 21:21:18 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var prefix = "b!"
 
 client.on('ready',() => {
   console.log("I\'m Online! Ready to launch!");
-  client.user.setActivity('!c7 help | discord.gg/67gbBKH', { type: 'PLAYING' })
+  client.user.setActivity('b!help | @akellicompany', { type: 'PLAYING' })
     .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'b! help'}`))
     .catch(console.error);
 })
@@ -13,7 +13,7 @@ client.on('ready',() => {
 client.on('message', message => {
   if (message.author === client.user) return;
   	if (message.content.startsWith(prefix + 'help')) {
-  		message.channel.send({embed: { "plainText": "", "title": "Bot Support", "description": "Commands: \n• b!help - Shows this message \n• servers - Displays clan & partnered servers \n •b!about - Shows bot information ", "color": 53380 }});
+  		message.channel.send({embed: { "plainText": "", "title": "Bot Support", "description": "Commands: \n• b!help - Shows this message \n• b!servers - Displays clan & partnered servers \n• b!about - Shows bot information ", "color": 53380 }});
   	}
     if (message.content.startsWith(prefix + 'servers')) {
   		message.channel.send({embed: { "plainText": "", "title": "Servers", "description": "Clan: \n• TeamCo7 Clan \nInvite: https://discord.gg/TW9yC4V \n• AkelliCompany \nInvite: https://discord.gg/j39xBQa \n \nPartnered Communities: \n• GoodBoy00's Lounge \nInvite: https://discord.gg/8yyfzYY \n• PrinceRaj 592 \nInvite: https://discord.gg/rGu7F39 \n• TheBestGamerYT: \nInvite: https://discord.gg/urSjkJx", "color": 53380 }});
